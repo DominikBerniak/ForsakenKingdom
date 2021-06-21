88d1c1a3e7949e0d0683a12be7d4622fecbab777
andy214 <mlocekjakub21@gmail.com>
2021-06-21 11:39:19 +0200

    implement create_item()

--- engine.py
+++ engine.py
@@ -1,3 +1,6 @@
+from os import name
+import random
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -24,3 +27,53 @@ def put_player_on_board(board, player):
     Nothing
     '''
     pass
+
+def random_item_name(type,type_description):
+    item = random.choice(type)
+    description_item = random.choice(type_description)
+    return description_item+" "+item
+
+def create_item():
+    MIN_ATTACK_VALUE = 1
+    MAX_ATTACK_VALUE = 10
+    MIN_ARMOR_VALUE = 1
+    MAX_ARMOR_VALUE = 10
+    MIN_GOLD_VALUE = 1
+    MAX_GOLD_VALUE = 17
+    MIN_CONSUMABLE_VALUE = 10
+    MAX_CONSUMABLE_VALUE = 50
+
+    item_stats = dict()
+    weapons = ["Bow","Warglaive","Staff","Wand","Axe","Sword","Mace","Dagger","Fist","Crossbow"]
+    weapons_description = ["Bloody","Blessed","Cursed","Doom","Big", "Metal" ,"War","Elvies","Small","Holy","Enchantend",""]
+    armor = ["Helmet", "Chest", "Trousers", "Shoes"]
+    armor_description = ["Plate","Leather","Mail","Cloth"]
+    consumable = ["Ham","Cheese","Elixir","Bread","Water"]
+    consumable_description = ["Stinky","Tasty","Godlike"]
+    type = ["weapons","weapons","weapons","armor","armor","consumable","gold"]
+
+    randomized_type = random.choice(type)
+    item_stats["type"] = randomized_type
+    if randomized_type == "weapons":
+        item_stats["name"] = random_item_name(weapons,weapons_description)
+        item_stats["value"] = random.randint(MIN_ATTACK_VALUE,MAX_ATTACK_VALUE)
+    elif randomized_type == "armor":
+        item_stats["name"] = random_item_name(armor,armor_description)
+        item_stats["value"] = random.randint(MIN_ARMOR_VALUE,MAX_ARMOR_VALUE)
+    elif randomized_type == "consumable":
+        item_stats["name"] = random_item_name(consumable,consumable_description)
+        item_stats["value"] = random.randint(MIN_CONSUMABLE_VALUE,MAX_CONSUMABLE_VALUE)
+    else:
+        item_stats["name"] = "Gold"
+        item_stats["value"] = random.randint(MIN_GOLD_VALUE,MAX_GOLD_VALUE)
+    return item_stats
+
+
+
+
+        
+        
+
+
+
+
